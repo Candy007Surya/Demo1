8ff0655dab1a33c08ffbcbc0a859961ba834fd03
Candy007Surya <saisurya17444@gmail.com>
2021-04-28 21:59:42 +0530

    naveen reddy changes

--- src/test/java/Cucumber/Automation/democlass.java
+++ src/test/java/Cucumber/Automation/democlass.java
@@ -7,5 +7,8 @@ public class democlass {
     System.Out.Println("surya");
      System.Out.Println("dad");
       System.Out.Println("mom");
+       System.Out.Println("naveen");
+        System.Out.Println("surya");
+
 
 }
