7f7a9d45a4b7257f6e1189282ab760e841ed3c60
Candy007Surya <saisurya17444@gmail.com>
2021-04-29 15:53:17 +0530

    very latest code from master branch is here--kkdev

--- src/test/java/Cucumber/Automation/democlass.java
+++ src/test/java/Cucumber/Automation/democlass.java
@@ -7,17 +7,27 @@ public class democlass {
     System.Out.Println("surya");
      System.Out.Println("dad");
       System.Out.Println("mom");
-<<<<<<< HEAD
+
        System.Out.Println("naveen");
         System.Out.Println("surya");
-=======
+
       System.Out.Println("eer");
       System.Out.Println("moefefm");
       System.Out.Println("2");
       System.Out.Println("4");System.Out.Println("3");
       System.Out.Println("5");
 
->>>>>>> qwerty
+      System.Out.Println("kkdev");
+
+      System.Out.Println("kkdev");
+
+      System.Out.Println("kkdev");
+
+      System.Out.Println("kkdev");
+
+      System.Out.Println("kkdev");
+
+
 
 
 }
